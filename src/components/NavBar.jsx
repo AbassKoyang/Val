a14dbc8388938ecce1abfe0a36fe8de4d3bd188c
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-12 00:48:42 +0930

    completd navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const NavBar = () => {
+    const location = useLocation();
+    console.log(location)
   return (
-    <header className="w-full px-2 py-2 md:px-4 md:py-3 lg:px-5 lg:py-4 bg-white drop-shadow-sm font-pacifico">
+    <header className="w-full px-2 py-2 md:px-4 md:py-3 lg:px-7 lg:py-6 bg-white drop-shadow-sm font-mont">
         <div className="w-full h-full max-w-[1400px] flex items-center justify-between">
-        <h1 className="text-5xl font-bold text-pink-600 font-pacifico leading-0">Val<span className="text-black text-7xl">.</span></h1>
-        <nav className="flex items-center gap-2 text-lg font-medium capitalize">
-            <Link to='/' aria-labelledby="Home">Home</Link>
-            <Link to='/' aria-labelledby="Home">E-cards</Link>
+        <h1 className="text-[38px] font-bold text-pink-600 font-pacifico leading-[0px]">beMyVal<span className="text-black text-[72px]">.</span></h1>
+        <nav className="flex h-full items-center gap-8 text-xl font-semibold">
+            <Link to='/' aria-labelledby="Home" className={`${location.pathname === '/' ? 'text-pink-600' : 'text-black/90'}`}>Home</Link>
+            <Link to='/' aria-labelledby="Home" className={`${location.pathname === '/home' ? 'text-pink-600' : 'text-black/90'}`}>E-cards</Link>
         </nav>
-        <button className="px-3 py-2 bg-pink-600 text-white text-lg font-medium rounded-3xl">Create cards</button>
+        <Link className="px-3 py-2 bg-pink-600 text-white text-lg font-semibold rounded-3xl">Create cards</Link>
         </div>
     </header>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <section className="w-full h-full">
+        <div className="tenor-gif-embed" data-postid="26865135" data-share-method="host" data-aspect-ratio="1.07744" data-width="100%"><a href="https://tenor.com/view/peach-goma-peach-and-goma-peach-peach-cat-sparkle-gif-26865135">Peach Goma Peach And Goma Sticker</a>from <a href="https://tenor.com/search/peach+goma-stickers">Peach Goma Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+    </section>
   )
 }
 
