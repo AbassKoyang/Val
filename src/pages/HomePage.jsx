cc5c14c09617a0ffea20ff55229f3d0498ec60ee
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-13 11:50:41 +0930

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import puppyDogGif from '../assets/peach-goma-peach-and-goma.gif'
 
 const HomePage = () => {
@@ -6,7 +7,7 @@ const HomePage = () => {
       <div className="col-span-1 h-full flex flex-col items-center justify-center px-3 lg:px-8">
             <h1 className='max-w-[100%] text-center text-4xl lg:text-6xl text-black font-pacifico'>Ready to Add Some Sparkle to Your Valentine's Day?</h1>
             <p className='mt-8 text-[16px] leading-[20px] text-center text-black/90 font-mont max-w-xl'>Say goodbye to boring Valentine's Day messages! 😂 With just a click of our magical button, you can charm your crush 😌, woo your sweetheart, 😍 or surprise your bestie with a burst of love and laughter 😜. Our whimsical button promises endless fun and excitement as you embark on a journey of romantic mischief. So why wait? Let's sprinkle some love and joy together! 💖✨</p>
-            <button className='px-4 py-1.5 text-lg md:text-xl text-white font-medium rounded-full mt-6 bg-pink-500'>Copy link</button>
+            <Link to='be-my-val' className='px-4 py-1.5 text-lg md:text-xl text-white font-medium rounded-full mt-6 bg-pink-500'>Copy link</Link>
         </div>
        <div className="col-span-1 flex flex-col items-center justify-center">
             <img src={puppyDogGif} alt="puppy-dog-eyes GIF" className="w-[400px]" />
