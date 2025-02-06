2e43321914721c6a9e14873eaf2c6d9e0d6003ae
Abass Koyang <abasskoyang12345@gmail.com>
2025-02-06 21:09:44 +0100

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
-
 const RootLayout = () => {
   return (
     <div className='w-full  bg-white overflow-x-hidden'>
@@ -9,7 +7,6 @@ const RootLayout = () => {
         <main className='w-full min-h-screen mb-5 p-3 bg-white overflow-x-hidden'>
             <Outlet />
         </main>
-        <Footer />
     </div>
   )
 }
